ecb69d568f476461958beb6e75731c372ee5a088
totoseriapovlup <tototester123456@gmail.com>
2022-04-21 18:37:31 +0300

    Update Index.php

--- vendor/Controllers/Index.php
+++ vendor/Controllers/Index.php
@@ -7,4 +7,8 @@ class Index
     public function index(){
         echo 'index controller index action';
     }
+    
+    public function store(){
+        echo 'index controller store method';
+    }
 }
