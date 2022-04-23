99226db1b6d637d22e2c915ad3332c33b62f7d91
toto <test@test.com>
2022-04-23 11:58:52 +0300

    views default template and index_index_page

---
+++ vendor/Views/index_index_page.php
@@ -0,0 +1,7 @@
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias amet at aut dolor dolores doloribus eaque
+    earum eligendi eos expedita fuga fugit hic, id impedit in laudantium minima natus necessitatibus neque numquam
+    officia officiis perspiciatis quis quisquam quod repellendus saepe similique soluta vel voluptatem voluptates
+    voluptatibus voluptatum. Aperiam eius exercitationem facere fuga in, maiores mollitia necessitatibus quasi quisquam
+    vitae. A consequatur corporis delectus facilis maiores qui vitae. Cupiditate dignissimos facilis id libero magni
+    natus, nulla quisquam quod reprehenderit vero.</p>
