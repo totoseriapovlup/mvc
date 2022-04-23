33886d4a0cc38214f9826a634579a94d865ee038
toto <test@test.com>
2022-04-23 11:00:48 +0300

    controller index remove unusable

--- vendor/Controllers/Index.php
+++ vendor/Controllers/Index.php
@@ -7,12 +7,4 @@ class Index
     public function index(){
         echo 'index controller index action';
     }
-
-    public function create(){
-        echo 'index controller create method';
-    }
-
-    public function store(){
-        echo 'index controller store method';
-    }
 }
